d03ccca91d2e06b8f2b9bcf4073267c8bc6c6843
Samyak Shah <samyakshah@biggie.westell.com>
2019-05-02 01:18:44 -0400

    Added functionality kNN for multiple inputs

--- k_nearest_neighbours.py
+++ k_nearest_neighbours.py
@@ -5,6 +5,8 @@ class kNN():
 	def __init__(self, k = 5):
 		self.k = k
 	def predict(self, x, X_train, y_train):
+		if len(x.shape) == 1:
+			x = x[np.newaxis, :]
 		'''Classify a new point according to KNN
 		----------
 		Inputs
@@ -16,11 +18,16 @@ class kNN():
 		Output
 		Class label of the new datapoint'''
 
-		dist_arr = np.sqrt(np.sum(np.square(x[np.newaxis, :] - X_train), axis = 1))
-		arr_sel = np.argsort(dist_arr)[:self.k] # argsort is from lowest to highest
-		unique, counts = np.unique(y_train[arr_sel], return_counts=True)
+#        dist_arr = np.sqrt(np.sum(np.square(x[np.newaxis, :] - X_train), axis = 1))
+		dist_arr = np.sqrt(np.sum(x**2, axis = 1).reshape(x.shape[0], 1) + np.sum(X_train**2, axis = 1) - 2*np.matmul(x, X_train.T))
+		y_pred = []
+		for i in range(len(x)):
+		    arr_sel = np.argsort(dist_arr[i])[:self.k] # argsort is from lowest to highest
+		    unique, counts = np.unique(y_train[arr_sel], return_counts=True)
 
-		return np.argmax(counts)
+		    y_pred.append(unique[np.argmax(counts)])
+
+		return y_pred
 
 if __name__=="__main__":
 	# Create Dataset 3 clusters
@@ -37,8 +44,13 @@ if __name__=="__main__":
 
 	cls = kNN(5)
 
-	x = np.array([0,0])
+	len_test = 10000
+	x = np.r_[np.random.multivariate_normal(mean_1, cov, len_test), np.random.multivariate_normal(mean_2, cov, len_test),np.random.multivariate_normal(mean_3, cov, len_test)]
+	y = np.array([0]*len_test + [1]*len_test + [2]*len_test)
+
 	lab = cls.predict(x, X_train, y_train)
 
-	print("New Data Point: {}".format(x))
-	print("Class Label: {}".format(lab))
+	print("Accuracy: {0:.2f}%".format(sum(y == lab)*100/len(lab)))
+
+	# print("New Data Point: {}".format(x))
+	# print("Class Label: {}".format(lab))
